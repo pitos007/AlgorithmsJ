208b57d0f3d64645e2d71a8e4b876bf979776455
Pitos <pitos007@gmail.com>
2017-02-20 16:55:30 +0000

    constrains

--- src/hackerRank17/JumpOnClouds.java
+++ src/hackerRank17/JumpOnClouds.java
@@ -5,32 +5,90 @@
  */
 package hackerRank17;
 
-/**
- *
- * @author patry
- */
+import java.util.List;
+import java.util.Scanner;
+
+
 public class JumpOnClouds {
     public static void main(String[] args) {
-        int[] cl = {0,0,1,0,0,1,0};
+        //int[] cl = {0,0,1,0,0,1,0};
+        int[] cl = {0,0,0,0,1,0};
         
+//        Scanner in = new Scanner(System.in);
+//        int cNum = in.nextInt();
+//        int[] cl = new int[cNum];
+//        for (int i = 0; i < cl.length; i++) {
+//            cl[i] = in.nextInt();
+//        }
+//        
+//        if (chk(cNum)) {
+//            if (chkFirst(cl)) {
+//                if (chkLoop(cl)) {
+//                    joc(cl);
+//                }
+//            }
+//        }
+
         joc(cl);
+        
     }
     
     public static void joc(int[] cl){
-        int pos = 0;
+        int i = 0;
         int count = 0;
-        for (pos = 0; pos < cl.length; pos++) {
-            if (cl[pos+2] == 1) {
-                pos++;
-                count++;
-                System.out.println("positio+1: " + pos + " ; counter: " + count);
+        while (i <= cl.length) {
+            if (i < cl.length-2) {
+                if (cl[i+2] == 1) {
+                    i++;
+                    count++;
+                    System.out.println("position+1: " + i + " ; counter: " + count);
+                }
+                else{
+                    i += 2;
+                    count++;
+                    System.out.println("position+2: " + i + " ; counter: " + count);
+                }
             }
             else{
-                pos += 2;
-                count++;
-                System.out.println("position+2: " + pos + " ; counter: " + count);
+                break;
             }
+            
         }
         System.out.println(count);
     }
-}
+    
+    public static boolean chk(int n){
+        boolean chk = false;
+        if ((n>=2)&&(n<=100)) {
+            //System.out.println(n + " within a range");
+            chk = true;
+        }
+        else{
+            //System.out.println(n + " too large or too small");
+        }
+        return chk;
+    }
+    
+    public static boolean chkLoop(int[] zeroOnes){
+        boolean chk = true;
+        for (int i = 0; i < zeroOnes.length; i++) {
+            if (!(zeroOnes[i]==0)&&!(zeroOnes[i]==1)) {
+                //System.out.println(zeroOnes[i] + " is too large or too small");
+                chk = false;
+                break;
+            }
+            else{
+                //System.out.println(zeroOnes[i] + " within a range");
+            }
+        }
+        return chk;
+    }
+    
+    public static boolean chkFirst(int[] zeroOnes){
+        boolean chk = true;
+        if (zeroOnes[0]==1) {
+            chk = false;
+        }
+        return chk;
+    }
+}   
